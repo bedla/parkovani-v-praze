1056dfc8dd5eab83fc21dfa09a8a3d674dd37a5f
Ivo Smid <ivo.smid@gmail.com>
2015-06-12 23:23:15 +0200

    Load .shp into H2Spatial DB

---
+++ shp-loader/src/main/java/cz/pragueparking/shploader/LoaderController.java
@@ -0,0 +1,53 @@
+package cz.pragueparking.shploader;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileSystemUtils;
+
+import java.io.File;
+
+@Controller
+@ConfigurationProperties(prefix = "my")
+public class LoaderController implements CommandLineRunner {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LoaderController.class);
+
+    @Value("${dbDataDir}")
+    private String dbDataDir;
+
+    @Value("${rootDataDir}")
+    private String rootDataDir;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        FileSystemUtils.deleteRecursively(new File(dbDataDir));
+
+        LOG.info("Creating Spatial alias");
+        jdbcTemplate.execute("CREATE ALIAS IF NOT EXISTS SPATIAL_INIT FOR \"org.h2gis.h2spatialext.CreateSpatialExtension.initSpatialExtension\";");
+
+        LOG.info("Initializing spatial extension");
+        jdbcTemplate.execute("CALL SPATIAL_INIT();");
+
+        loadData("DOP_ZPS_Stani_l_mercator.shp", "DOP_ZPS_Stani_l");
+        loadData("DOP_ZachParkoviste_b_mercator.shp", "DOP_ZachParkoviste_b");
+        loadData("DOP_ZachParkoviste_b_mercator-buffer20.shp", "DOP_ZachParkoviste_b-buffer20");
+        loadData("DOP_ZPS_Automaty_b_mercator.shp", "DOP_ZPS_Automaty_b_mercator");
+        loadData("DOP_ZPS_Automaty_b_mercator-buffer20.shp", "DOP_ZPS_Automaty_b-buffer20");
+    }
+
+    private void loadData(String file, String tableName) {
+
+        LOG.info(String.format("Loading spatial data for %s into table %s", file, tableName));
+        jdbcTemplate.execute(String.format("CALL FILE_TABLE('%s', '%s');", rootDataDir + "/" + file, tableName));
+    }
+}
---
+++ shp-loader/src/main/java/cz/pragueparking/shploader/ShapeFileLoader.java
@@ -0,0 +1,18 @@
+package cz.pragueparking.shploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@EnableAutoConfiguration
+@SpringBootApplication
+@ConfigurationProperties(prefix = "my")
+public class ShapeFileLoader {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ShapeFileLoader.class, args);
+    }
+}
