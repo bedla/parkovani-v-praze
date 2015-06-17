3ba3365eae516366abdfef3fc370a4f1d3ab42f7
Ivo Smid <ivo.smid@gmail.com>
2015-06-17 22:18:13 +0200

    Allow deploy on servlet container

--- web/src/main/java/cz/pragueparking/web/ParkingApplication.java
+++ web/src/main/java/cz/pragueparking/web/ParkingApplication.java
@@ -3,16 +3,27 @@ package cz.pragueparking.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
-@ComponentScan
-public class ParkingApplication extends WebMvcAutoConfiguration {
+public class ParkingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(ParkingApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Config.class);
+    }
+
+    @EnableWebMvc
+    @ComponentScan
+    public static class Config extends WebMvcAutoConfiguration {
+
     }
 
 }
