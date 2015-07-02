245eb04c0ef5e26607ce6e65efb6bbe6809daf24
Ivo Smid <ivo.smid@gmail.com>
2015-07-02 16:31:56 +0200

    Shutdown database to prevent opened .db files

--- web/src/main/java/cz/pragueparking/web/ParkingApplication.java
+++ web/src/main/java/cz/pragueparking/web/ParkingApplication.java
@@ -1,6 +1,9 @@
 package cz.pragueparking.web;
 
 import cz.pragueparking.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
@@ -8,6 +11,12 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.annotation.PreDestroy;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 @SpringBootApplication
 public class ParkingApplication extends SpringBootServletInitializer {
@@ -30,6 +39,32 @@ public class ParkingApplication extends SpringBootServletInitializer {
     @ComponentScan
     public static class Config extends WebMvcAutoConfiguration {
 
+        private static final Logger LOG = LoggerFactory.getLogger(Config.class);
+
+        @Autowired
+        private DataSource dataSource;
+
+        @PreDestroy
+        public void close() {
+
+            dbShutdown();
+            if (dataSource instanceof org.apache.tomcat.jdbc.pool.DataSource) {
+                ((org.apache.tomcat.jdbc.pool.DataSource) dataSource).close(true);
+            }
+        }
+
+        /**
+         * Shutdown database to prevent opened .db files
+         */
+        private void dbShutdown() {
+            try (Connection connection = dataSource.getConnection()) {
+                try (Statement statement = connection.createStatement()) {
+                    statement.execute("SHUTDOWN");
+                }
+            } catch (SQLException e) {
+                LOG.error("Unable to shutdown H2 database", e);
+            }
+        }
     }
 
 }
